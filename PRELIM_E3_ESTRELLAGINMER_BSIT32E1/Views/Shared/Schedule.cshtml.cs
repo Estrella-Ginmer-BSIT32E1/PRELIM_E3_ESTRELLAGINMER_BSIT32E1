a76f0982840e164dc6aa96c8107db0cb368e0b37
STUDENT <STUDENT@DESKTOP-5L8Q2MB>
2024-03-02 10:00:16 +0800

    Add project files.

---
+++ PRELIM_E3_ESTRELLAGINMER_BSIT32E1/Views/Shared/Schedule.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRELIM_E3_ESTRELLAGINMER_BSIT32E1.Views.Shared
+{
+    public class ScheduleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
